5606bdcd36d1c27ec0a76fa232d2378e18b41c2d
unknown <wadhera@WADHERA4.americas.hpqcorp.net>
2015-09-03 19:23:25 -0700

    Create Maven Project

---
+++ src/main/java/com/neha/project/App.java
@@ -0,0 +1,13 @@
+package com.neha.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
